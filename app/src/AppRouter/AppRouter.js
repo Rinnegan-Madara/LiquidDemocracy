aa418c40739fba7c1188c1ea1a480f8900e01a45
Sierra-117 <abhinavkinshuk@gmail.com>
2019-04-22 00:37:25 +0530

    router setup successful

--- app/src/AppRouter/AppRouter.js
+++ app/src/AppRouter/AppRouter.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { DrizzleContext } from 'drizzle-react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 import Dashboard from '../components/Dashboard';
 import AddMember from '../components/AddMember';
@@ -8,15 +8,17 @@ import AddMember from '../components/AddMember';
 const AppRouter = (props) => (
     <DrizzleContext.Consumer>
     {drizzleContext => {
-        console.log(drizzleContext);
+        {/* console.log(drizzleContext); */}
         const { initialized, drizzleState, drizzle } = drizzleContext;
         return (
             <BrowserRouter>
                 <Fragment>
                     <Header />
                     <Switch>
-                        <Route path='/' component={Dashboard} exact={true} />
-                        <Route path='/addMember' component={AddMember} exact={true} />
+                        <Route path='/' render={Dashboard} exact={true} />
+                        <Route path='/addMember' 
+                            render={()=>(<AddMember drizzle={drizzle} drizzleState={drizzleState}/>)} 
+                            exact={true} />
                     </Switch>
                 </Fragment>
             </BrowserRouter>
