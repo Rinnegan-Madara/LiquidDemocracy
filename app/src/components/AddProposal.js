20623259fb210991391d2056e6726ee1677738df
Sierra-117 <abhinavkinshuk@gmail.com>
2019-04-22 21:18:42 +0530

    completed add proposals

--- app/src/components/AddProposal.js
+++ app/src/components/AddProposal.js
@@ -2,9 +2,10 @@ import React, { Component, Fragment } from 'react';
 
 export default class AddProposal extends Component{
     state = {
-        web3:{},
-        contract:{},
-        isMember:false
+        isMember:false,
+        text:'',
+        expiry:'',
+        txId:''
     }
     componentDidMount(){
         const { drizzleState } = this.props;
@@ -17,15 +18,56 @@ export default class AddProposal extends Component{
             (error,result) => {
                 this.setState({isMember:result});
             })
+    }
+    handleTextChange = e =>{
+        this.setState({text:e.target.value});
+    }
+    handleExpiryChange = e =>{
+        this.setState({expiry:e.target.value*86400});
+    }
+    handleSubmit = e => {
+        e.preventDefault();
+        const { drizzle, drizzleState } = this.props;
+        const { text, expiry } = this.state;
+        const { LiquidDemocracy } = drizzle.contracts;
+        console.log(LiquidDemocracy);
 
-        this.setState({web3,contract});
+        const txId = LiquidDemocracy
+            .methods['addProposal']
+            .cacheSend(
+                text,
+                expiry,
+                {from: drizzleState.accounts[0]}
+            );
+        this.setState({txId});
     }
     render(){
-        const { isMember, web3, contract } = this.state;
+        const { isMember, txId } = this.state;
+        const { transactions, transactionStack } = this.props.drizzleState;
+        const txHash = transactionStack[txId];
         return(
             <Fragment>
                 Add Proposals
-                {isMember ? <div>hello</div> : <div>You cannot add a proposal</div>}
+                {isMember ?
+                <Fragment>
+                <form onSubmit={this.handleSubmit}>
+                    <p>Enter your proposal here</p>   
+                    <textarea onChange={this.handleTextChange} required></textarea><br/>
+                    <label htmlFor="expiry">Enter duration of validity, starting now, in days:</label>
+                    <input type="number" 
+                        onChange={this.handleExpiryChange}
+                        defaultValue="0"
+                        min="0"
+                        required/>     
+                        <br/>
+                    <button type="submit">Submit</button>
+                </form>
+                <p>{txHash ? `Transaction status: ${transactions[txHash]
+                    && transactions[txHash].status}`: null}</p>
+                </Fragment>
+                 : 
+                 <div>You are not a member and cannot add a proposal</div>
+                }
             </Fragment>
         )
     }
